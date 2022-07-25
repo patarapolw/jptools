70c1140040d0f29732b67f68b38c51e0fbfa4209
Pacharapol Withayasakpunt <polv@polv.cc>
2022-07-25 17:51:39 +0700

    add joyo set

--- packages/kanji-concluder/src/index.ts
+++ packages/kanji-concluder/src/index.ts
@@ -1,4 +1,28 @@
 import { readFileSync } from 'fs';
 import yaml from 'js-yaml';
 
-const x = 1;
+const reJa = /\p{sc=Han}/gu;
+
+async function main() {
+  const txt = readFileSync('assets/Being Japanese Part 1 of 2.md', 'utf-8');
+
+  const chars = new Map<string, number>();
+  reJa.lastIndex = 0;
+  while (true) {
+    const m = reJa.exec(txt);
+    if (!m) {
+      break;
+    }
+
+    const c = m[0];
+    if (!c) {
+      break;
+    }
+
+    chars.set(c, (chars.get(c) || 0) + 1);
+  }
+
+  console.log(Array.from(chars).sort(([, i], [, j]) => j - i));
+}
+
+main();
